4007fa83d7584d11fd8bbb11e855019f6724d72b
nikkjonee <nikkjone05@gmail.com>
2025-06-20 16:48:17 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+project = 'totaldefense-install'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_favicon = '_static/totaldefense.png'
+
+html_js_files = [
+    'livechat.js',
+]
